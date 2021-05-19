b57b0a06b12c2e4dfb19a747e7d180a75bd50f43
NoOourAn <nouranaborwash@gmail.com>
2021-05-19 23:38:00 +0200

    End of Walking Skeleton FrontEnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+
+  const [activities , setActivities] = useState([]);   
+  //[] is initial state for activities value
+
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/activities")
+    .then(res=>{
+      console.log(res)
+      setActivities(res.data)
+    })
+  },[])   ///the deps list is empty to prevent rerender (just run once)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header as='h2' content='Reactivities' color='yellow' icon='plug'/>
+        <List>
+          {activities.map( (a:any) => (
+              <List.Item key={a.id}>
+                {a.title}
+              </List.Item>
+            )
+          )}
+        </List>
+        
       </header>
     </div>
   );
